c031af90112a75af2da322e5396bb7e1bab8cc22
xushilei <xushilei@66692c61-3049-0410-ab2b-097abb7809bd>
2008-03-24 17:31:12 +0000

    bridge + misc things... haven't started client side

---
+++ trunk/Flibble/src/sg/edu/ntu/Flibble/Constant.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.Flibble;
+
+public class Constant {
+	public class Application {
+		public static final String bridgePropertyFile = "/WEB-INF/bridge.property";
+		public static final String userPropertyFile = "/WEB-INF/user.property";
+		public static final String flickrConnString = "http://api.flickr.com/services/rest/?method=flickr.photos.getRecent";
+		public static final String flickrApiKey = "api_key";
+		public static final String flickrExtras = "extras";
+		public static final String flickrPerPage = "per_page";
+		public static final String flickrPage = "page=1";
+	}
+}
---
+++ trunk/Flibble/src/sg/edu/ntu/Flibble/FlibbleBridge.java
@@ -0,0 +1,76 @@
+package sg.edu.ntu.Flibble;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class for Servlet: FlibbleBridge
+ *
+ */
+ public class FlibbleBridge extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+   static final long serialVersionUID = 1L;
+   private static final int bridgeMultiFactor = 3;
+   private Properties properties;
+   
+   
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public FlibbleBridge() {
+		super();
+	}   	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}  	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = Constant.Application.flickrConnString 
+			+ "&" + Constant.Application.flickrApiKey + "=" + properties.getProperty("FlibbleDefaultApiKey")
+			+ "&" + Constant.Application.flickrExtras + "=" + properties.getProperty("FlibbleDefaultExtras")
+			+ "&" + Constant.Application.flickrPerPage + "=" 
+			+ (Math.pow(Integer.parseInt(properties.getProperty("FlibbleDefaultMatrixSize")), 2)*2)
+			+ "&" + Constant.Application.flickrPage;
+		
+		//i copy yuku's proxy lab here :D
+		HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+		InputStreamReader in = new InputStreamReader(con.getInputStream(), "utf-8");
+		response.setContentType(con.getContentType());
+		while(true) {
+			int c = in.read();
+			if (c < 0) break;
+			response.getWriter().print((char) c);
+		}
+	}   	  	  
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#init()
+	 */
+	public void init() throws ServletException {
+		InputStream in = null;
+		try {
+		System.out.println("about to get file:" + Constant.Application.bridgePropertyFile);
+	    in = getServletContext().getResourceAsStream(Constant.Application.bridgePropertyFile);
+	    properties = new Properties();
+	    properties.load(in);
+	    super.init();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}   
+}
